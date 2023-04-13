f4ec66516cf2ba3b85506a15ae677b034b2b6a4f
Vinayak Tripathi <66787122+vinayak-tripathi@users.noreply.github.com>
2023-04-13 12:53:44 +0530

    13Apr2023- News Update

--- editor/views.py
+++ editor/views.py
@@ -2,6 +2,7 @@ import time
 from django.shortcuts import get_object_or_404, render, redirect
 from editor.forms import StateForm, MinistryForm
 from schemes.models import Schemes, Ministry, State, Category
+from news.models import News
 from django.db.models import Q
 from django.urls import reverse_lazy
 # Create your views here.
@@ -138,7 +139,7 @@ class SchemeAdd(LoginRequiredMixin,CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(SchemeAdd,self).form_valid(form)
-    
+   
 class StateAdd(LoginRequiredMixin,View):
 
     def get(self,request):
@@ -252,3 +253,89 @@ class GeeksDeleteView(LoginRequiredMixin,DeleteView):
     success_url = reverse_lazy('updatescheme')
      
     template_name = "editor/confirm_delete.html"
+
+class NewsUpdateListView(LoginRequiredMixin,ListView):
+    model = News
+    template_name = 'editor/update_news_list.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'news'
+    ordering = ['-date']
+    paginate_by = 8
+    queryset = model.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # search_input = self.request.GET.get('search-area') or ''
+        context['title'] = 'Update News'
+        # context['search_input']= search_input
+        # print(context)
+        return context
+    # def get_queryset(self):
+    #     # user = get_object_or_404(User, username=self.kwargs.get('username'))
+    #     search_input = self.request.GET.get('search-area') or ''
+    #     return Schemes.objects.filter(Q(details__icontains=search_input)|Q(eligibility__icontains=search_input)).order_by('-uploadDate')
+
+
+class NewsAdd(LoginRequiredMixin,CreateView):
+    model = News
+    template_name = 'editor/schemes_form.html'
+    fields = ['headline','details','date','source','tags','image']
+    # exclude_fields = ['slug','uploadDate']
+    # fields = ['title','name','state','brief','ministry','eligibility','references','tags','details','category','openDate','closeDate','image']
+    # category = Tags.objects.all()
+    # success_url = reverse_lazy('tasks')
+    success_url = reverse_lazy('editor')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # context['category'] = Category.objects.all()
+        context['title']='Add News'
+        # search_input = self.request.GET.get('search-area') or ''
+        # context['title'] = 'Update Schemes'
+        # context['search_input']= search_input
+        # # print(context)
+        return context
+    def get_form(self,form_class=None):
+        form = super(NewsAdd,self).get_form(form_class)
+        # form.fields['category'].widget.attrs.update({"data-role":"tagsinput"})
+        form.fields['tags'].widget.attrs.update({"data-role":"tagsinput"})
+        # form.fields['subcategory'].widget.attrs.update({"data-role":"tagsinput"})
+        form.fields['date'].widget = DateInput()
+        # form.fields['uploadDate'].widget = DateInput()
+        # form.fields['closeDate'].widget = DateInput()
+        return form
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(NewsAdd,self).form_valid(form)
+    
+class NewsUpdate(LoginRequiredMixin,UpdateView):
+    model = News
+    template_name = 'editor/schemes_update_form.html'
+    fields = ['headline','details','date','source','tags','image']
+    success_url = reverse_lazy('updatenews')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Edit News"
+        context['slug'] = self.object.slug
+        return context
+    def get_form(self,form_class=None):
+        form = super(NewsUpdate,self).get_form(form_class)
+        # form.fields['category'].widget.attrs.update({"data-role":"tagsinput"})
+        form.fields['tags'].widget.attrs.update({"data-role":"tagsinput"})
+        # form.fields['subcategory'].widget.attrs.update({"data-role":"tagsinput"})
+        form.fields['date'].widget = DateInput()
+        return form
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(NewsUpdate,self).form_valid(form)
+    
+class NewsDeleteView(LoginRequiredMixin,DeleteView):
+    # specify the model you want to use
+    model = News
+     
+    # can specify success url
+    # url to redirect after successfully
+    # deleting object
+    success_url = reverse_lazy('updatenewsList')
+     
+    template_name = "editor/confirm_delete.html"
+
